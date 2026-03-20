cc2622afa2f618d4f71bf2be0d2c4f43ba2b6892
arpankaushal05 <arpankaushal23@gmail.com>
2026-03-20 09:19:50 +0530

    UC1 - Application Entry & Welcome Message

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,5 +1,7 @@
 class Palindrome {
-    public static void main(String[] args) {
-      System.out.println("");
-    }
-}
+    // UC1
+public static void main(String [] args){
+    System.out.println("=== Palindrome Checker App ===");
+    System.out.println("Version 1.0 - Complete Implementation");
+    System.out.println("Welcome Message");
+}}
